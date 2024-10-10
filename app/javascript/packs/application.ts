1ef2aea4abbba7dfc2d69a22301da2de4e4bcff4
pcktbot <pcktbot@gmail.com>
2024-10-09 19:36:38 -0500

    good lord, I was close tho

---
+++ app/javascript/packs/application.ts
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import * as Rails from "@rails/ujs"
+import * as Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+// Your custom TypeScript code can go here
+console.log("Hello from TypeScript!")
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,25 +1,24 @@
-import path from 'path';
+import * as path from 'path';
 import {EsbuildPlugin} from 'esbuild-loader';
 import {Configuration} from 'webpack';
 
 const config: Configuration = {
-  entry: {
-    application: './app/javascript/application.ts',
-  },
-  output: {
-    filename: 'application.js',
-    path: path.resolve(__dirname, './public')
-  },
-  resolve: {extensions: ['.ts', '.js'],
-            modules:    [path.resolve(__dirname, 'app/javascript'), 'node_modules']
-  },
-  module: {rules: [{  test: /\.ts$/,
+  entry:    {application: './app/javascript/application.ts',
+            },
+  output:   {filename: '[name].js',
+             path: path.resolve(__dirname, 'app/assets/builds')
+            },
+  resolve:  {extensions: ['.ts', '.js'],
+             modules:    [path.resolve(__dirname, 'app/javascript'),
+                         'node_modules']
+            },
+  module:   {rules: [{test: /\.ts$/,
                       loader: 'esbuild-loader',
-                      options: {loader: 'ts', target: 'es2015'}}]
-          },
-  plugins: [new EsbuildPlugin()],
-  mode: 'development',
-  
+                      options: {loader: 'ts', target: 'es2015'}
+                    }]
+            },
+  plugins:  [new EsbuildPlugin()],
+  mode:     'development',
 };
 
 export default config;
